d44ed6ae0d343b1b1fc4df00cf0780a15c75cc39
Pablo Cantero <pablo@pablocantero.com>
2013-03-16 16:16:47 -0300

    Ignores refinements for non-ruby2; Adds .rvmrc to use Ruby 2.0.0 by default

--- src/string_spelling_corrector_refinement.rb
+++ src/string_spelling_corrector_refinement.rb
@@ -1,11 +1,18 @@
-require_relative "spelling_corrector"
+if RUBY_VERSION.to_i == 2
 
-module StringSpellingCorrectorRefinement
-  refine String do
-    def correct
-      @corrector ||= SpellingCorrector.new
-      @corrector.correct self
+  require_relative "spelling_corrector"
+
+  module StringSpellingCorrectorRefinement
+    refine String do
+      def correct
+        @corrector ||= SpellingCorrector.new
+        @corrector.correct self
+      end
     end
   end
+
+  # Example
+  # using StringSpellingCorrectorRefinement
+  # "cen".correct => "can"
 end
 
